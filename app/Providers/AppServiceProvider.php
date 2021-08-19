93e51ede815c98d3555a4183d944fc831356b39f
Ehsan Dastras <ehsan1372d@gmail.com>
2021-08-19 00:06:33 -0700

    Using 3 option of view composer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,9 +5,12 @@ namespace App\Providers;
 use App\Http\AuthWays\AuthWaysContracts;
 use App\Http\AuthWays\GithubOauth;
 use App\Http\AuthWays\GoogleOAuth;
+use App\Http\Views\Composers\ChannelComposer;
+use App\Models\Channel;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -41,5 +44,19 @@ class AppServiceProvider extends ServiceProvider
             });
         }
 
+        //every single page
+        //View::share('channels',Channel::orderBy('title')->get());
+
+        //Granular
+
+        //View::composer(['channels.index','posts.create'] , function ($view){
+        //   $view->with('channels',Channel::orderBy('title')->get());
+       // });
+
+        //Composer Classes
+        //View::composer(['channels.index','posts.create'],ChannelComposer::class);
+
+        //Use Partials with Composer Classes
+        View::composer('partials.*',ChannelComposer::class);
     }
 }
